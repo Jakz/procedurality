730923bc628d56350bbcbe33b8d185685ed9d9c9
Jack <jack@pixbits.com>
2025-05-25 01:25:51 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include "raylib.h"
+
+int main(int argc, char *argv[])
+{
+  SetConfigFlags(FLAG_MSAA_4X_HINT);
+  
+  InitWindow(1920, 1080, "Procedurality");
+  Image img = GenImagePerlinNoise(256, 256, 0, 0, 10.0f);
+  Texture2D tex = LoadTextureFromImage(img);
+
+  SetTargetFPS(60);
+
+  Mesh mesh = GenMeshTorus(1.0f, 0.25f, 16, 32);
+  Model model = LoadModelFromMesh(mesh);
+
+  Camera camera = { 0 };
+  camera.position = { 4.0f, 2.0f, 1.0f };  // Camera position
+  camera.target = { 0.0f, 0.0f, 0.0f };    // Look at the origin
+  camera.up = { 0.0f, 1.0f, 0.0f };        // Up vector
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
+
+  while (!WindowShouldClose())
+  {
+    UpdateCamera(&camera, CAMERA_ORBITAL);
+    
+    BeginDrawing();
+    ClearBackground(Color{0, 12, 31});
+    DrawTexture(tex, 0, 0, WHITE);
+
+    BeginMode3D(camera);
+    DrawModelWires(model, { 0, 0, 0 }, 1.0f, RED);
+    EndMode3D();
+
+    EndDrawing();
+  }
+}
